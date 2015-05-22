2534aa219ffb0ff88de70297b00848e4734f6916
Alex Whitney <awhitney@cvent.com>
2015-05-22 11:54:51 -0400

    list instances + ip addresses in elasticbeanstalk:describeEvnrionments

--- helpers/elasticBeanstalk/describeEnvironments.js
+++ helpers/elasticBeanstalk/describeEnvironments.js
@@ -1,6 +1,7 @@
 var aws = require('aws-sdk'),
     _ = require('underscore'),
-    grunt = require('grunt');
+    grunt = require('grunt'),
+    Promise = require('bluebird');
 
 function colorize(colorName) {
     var code = {
@@ -13,19 +14,58 @@ function colorize(colorName) {
 }
 
 module.exports = function (config, options, done) {
-    (new aws.ElasticBeanstalk()).describeEnvironments({
-        ApplicationName: options.beanstalk.applicationName
-    }, function (err, data) {
+    var environments;
+    new Promise(function (resolve, reject) {
+        (new aws.ElasticBeanstalk()).describeEnvironments({
+            ApplicationName: options.beanstalk.applicationName
+        }, function (err, data) {
+            if (err) { return reject(err); }
 
-        _.each(data.Environments, function (env) {
-            grunt.log.writeln(env.EnvironmentName + ' [' + env.EnvironmentId + ']');
-            grunt.log.writeln('  Status: ' + env.Status);
-            grunt.log.writeln('  Health: ' + colorize(env.Health));
-            grunt.log.writeln('  Version: ' + env.VersionLabel);
-            grunt.log.writeln('  URL: ' + env.CNAME);
+            environments = data.Environments;
+
+            resolve(environments);
+        });
+    }).map(function (env) {
+        return new Promise(function (resolve, reject) {
+            (new aws.ElasticBeanstalk()).describeEnvironmentResources({
+                EnvironmentName: env.EnvironmentName
+            }, function (err, data) {
+                if (err) {
+                    return reject(err);
+                }
+
+                (new aws.EC2()).describeInstances({
+                    InstanceIds: _.pluck(data.EnvironmentResources.Instances, 'Id')
+                }, function (err, data) {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    env._instances = data.Reservations;
+
+                    resolve(env);
+                });
+            });
+        });
+    }).map(function (env) {
+        grunt.log.writeln(env.EnvironmentName + ' [' + env.EnvironmentId + ']');
+        grunt.log.writeln('  Status: ' + env.Status);
+        grunt.log.writeln('  Health: ' + colorize(env.Health));
+        grunt.log.writeln('  Version: ' + env.VersionLabel);
+        grunt.log.writeln('  URL: ' + env.CNAME);
+
+        grunt.log.writeln('  Instances:');
+        _.each(env._instances, function (inst) {
+            inst = inst.Instances[0];
+            grunt.log.write('    ' + inst.InstanceId + ': ');
+            grunt.log.write('[' + inst.State.Name + '] ');
+            grunt.log.write(inst.PrivateIpAddress + ' ');
+            if (inst.PublicIpAddress) {
+                grunt.log.write('(pub: ' + inst.PublicIpAddress + ') ');
+            }
             grunt.log.writeln();
         });
 
-        done();
-    });
+        grunt.log.writeln();
+    }).nodeify(done);
 };
